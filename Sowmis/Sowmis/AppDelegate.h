6f010a586ee85990f47f6b5e8e1c2ac8538fadbd
hanjin@baihe.com <hanjin@baihe.com>
2015-07-24 19:05:07 +0800

    小秘书
    创建工程 上传代码文件

---
+++ Sowmis/Sowmis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sowmis
+//
+//  Created by Jade on 15/7/24.
+//  Copyright (c) 2015年 韩瑾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sowmis/Sowmis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sowmis
+//
+//  Created by Jade on 15/7/24.
+//  Copyright (c) 2015年 韩瑾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
